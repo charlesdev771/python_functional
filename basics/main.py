74f6c05ab976d8f396de513d1e0ecdda16f675d3
charlesdev771 <charlesdev07721@proton.me>
2022-11-26 00:06:14 -0300

    First

---
+++ basics/main.py
@@ -0,0 +1,13 @@
+class My_main:
+
+    def __init__(self, my, at):
+
+        self.my = my
+        self.at = at
+
+    def menuMain(self, my):
+
+        print(self.my)
+
+my_main = My_main(1, 2)
+my_main.menuMain(1)
